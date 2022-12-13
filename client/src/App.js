d066ee05e5215288179643b435488dd3e961ece6
Dary <dady281100@gmail.com>
2022-12-13 00:27:47 -0600

    Home page added and NoMatch Router added

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route 
+                path="/" 
+                element={<Home />} 
+              />
+              <Route 
+                path="*" 
+                element={<NoMatch />} 
+              />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
   );
 }
 
---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoMatch = () => {
+    return (
+        <div>
+            Sorry, Page not found.
+        </div>
+    );
+};
+
+export default NoMatch;
